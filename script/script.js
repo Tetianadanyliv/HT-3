acf556473c738669d9fa4a5f8dabff9d64246fc9
Tetianadanyliv <tanyadanuliv@gmail.com>
2022-12-27 19:10:08 +0200

    HT-3

--- script/script.js
+++ script/script.js
@@ -15,17 +15,41 @@ function getPower () {
 let a = Number(prompt("Введіть число для обчислення степені"));
 let b = Number(prompt("Введіть число для підняття у степінь"));
   let result = a;
+  if (b === 0) {
+   return alert(`Результат = 1`);
+  } 
   for (let i = 1; i < b; i++){
     result *= a; 
+   return alert(`Результат = ${result}`);
   }
-   alert(result);
+  for (let i = 1; i > b; i++){
+    result = 1/a; 
+   return alert(`Результат = ${result}`);
+  }
 }
+
+
+// function getPower () {
+//   let a = Number(prompt("Введіть число для обчислення степені"));
+//   let b = Number(prompt("Введіть число для підняття у степінь"));
+//     let result = a;
+//     for (let i = 1; i < b; i++){
+//       result *= a; 
+//     }
+//      alert(result);
+//   }
+  
+
+
 
   /*Створити функцію, яка форматує ім'я, роблячи першу букву великою. ("влад" ->
 "Влад", "вЛАД" -> "Влад" і так далі);*/
+ 
 
 function capFirstLetter(string) {
-  alert(string.charAt(0).toUpperCase() + string.slice(1));
+  string = prompt("Введіть своє ім'я").toLowerCase();
+  const Namefix = string[0].toUpperCase() + string.slice(1);
+  alert (Namefix)
 }
 
 
@@ -35,7 +59,8 @@ function capFirstLetter(string) {
 function countTax () {
   let salary = Number(prompt("Введіть суму заробітньої плати"));
   let tax = 19.5;
-  alert(salary - (salary/100 *tax).toFixed(0));
+  let salarytax = salary-(salary/100 *tax).toFixed(0);
+  alert (`Сума зарплати після сплати податку становить ${salarytax} грн.`);
 }
 
 
@@ -43,7 +68,9 @@ function countTax () {
 function getRandomNumber () {
   let numberN = Number(prompt("Введіть значення N"));
   let numberM = Number(prompt("Введіть значення M"));
-  alert(Math.floor(Math.random() * (numberM - numberN)) + numberN);
+  
+  let rendomResalt = (Math.floor(Math.random() * (numberM - numberN)) + numberN);
+  alert (`Випадкове число ${rendomResalt}`)
 }
 
 /*Створити функцію, яка рахує скільки разів певна буква повторюється в слові.
@@ -68,22 +95,27 @@ function countLetters() {
 // грн. або convertCurrency("2500UAH") -> 100$*/
 
 function convertCurrency() {
-  const  amount = prompt("Введіть валюту");
+  let  amount = prompt("Enter the currency").toUpperCase();
   let convertedAmount = 0;
   if(amount.indexOf('$') > 0) {
     convertedAmount = parseInt(amount) * 25;
+    alert (`The exchange amount is ${convertedAmount} uah`)
   } else if (amount.indexOf('UAH') > 0) {
     convertedAmount = parseInt(amount) / 25;
+    alert (`The exchange amount is ${convertedAmount} $`)
   }
-  alert(convertedAmount);
+  else {
+   return alert (convertedAmount = 'Your currency is invalid!');
+  }
 }
+
 
 // /*Створіть функцію генерації випадкового паролю (тільки числа), довжина
 // встановлюється користувачем або по замовчуванню = 8 символам.
 // Приклад: getRandomPassword(4) -> 1875, getRandomPassword() -> 87240124*/
 
 function getRandomPassword() {
-  const code = prompt("Введіть довжину паролю не більше числа 8");
+  const code = prompt("Введіть число довжини паролю");
   let password = '';
   let i = code || '8';
   while(i > 0) {
@@ -108,17 +140,16 @@ function deleteLetter() {
 // isPalyndrom("мадам") -> true, isPalyndrom("кокос") -> false, isPalyndrom("Я несу
 // гусеня") -> true*/
 
-
 function isPalindrome() {
   const string = prompt('Enter a string: ');
   const len = string.length;
-  for (let i = 0; i < len / 2; i++) {
+  for (let i = 0; i < len/2; i++) {
       if (string[i] !== string[len - 1 - i]) {
-          alert('It is not a palindrome');
-      }
-  }
-  alert('It is a palindrome');
-}
+        return alert("It's not a palindrom");
+      } else return alert("It is a palindrom");
+    }
+  };
+
 
 
 
